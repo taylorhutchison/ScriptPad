3060e753fe244750bb6f3d856fc63212c5fcb794
Taylor Hutchison <seanthutchison@gmail.com>
2016-06-04 16:37:06 -0500

    Wiring up event listeners for grabbar.

--- src/app/components/grabbar.component.js
+++ src/app/components/grabbar.component.js
@@ -15,24 +15,49 @@ let GrabBarComponent = class GrabBarComponent {
         this.classMap = { 'vertical': false, 'horizontal': false, grabbing: false };
     }
     ngOnInit() {
-        var orient = this.orientation.toLowerCase();
-        if (orient == 'vertical' || orient == 'horizontal') {
-            this.classMap[orient] = true;
+        this.orient = this.orientation.toLowerCase();
+        console.log('setting orient to ' + this.orient);
+        if (this.orient == 'vertical' || this.orient == 'horizontal') {
+            this.classMap[this.orient] = true;
         }
         else {
             throw new Error("Grab bar orientation " + this.orientation + " is not an acceptable value. Only 'vertical' and 'horizontal' are accepted.");
         }
     }
-    startGrab() {
+    startGrab(event) {
         this.classMap.grabbing = true;
+        if (this.orient == 'vertical') {
+            this.startPos = event.target.offsetWidth;
+        }
+        else {
+            this.startPos = event.target.offsetHeight;
+        }
+        this.previousElement = event.target.parentElement.previousElementSibling;
+        console.log(this.previousElement);
+        this.setMouseListeners(event);
     }
     endGrab() {
         this.classMap.grabbing = false;
     }
-    getSiblings(event) {
-        var grabbar = event.target.parentElement;
-        var previousElement = grabbar.previousElementSibling;
-        var nextElement = grabbar.nextElementSibling;
+    setMouseListeners(event) {
+        var self = this;
+        var body = document.getElementsByTagName('body')[0];
+        var mouseMoveListener = function (event) {
+            if (self.orient == 'vertical') {
+                self.previousElement.style.width = event.pageX + 'px';
+            }
+            else {
+                self.previousElement.style.height = event.pageY + 'px';
+            }
+        };
+        var mouseUpListener = function (event) {
+            body.removeEventListener('mousemove', mouseMoveListener);
+            body.removeEventListener('mouseup', mouseUpListener);
+            self.endGrab();
+            console.log("mouseUp");
+        };
+        body.addEventListener('mousemove', mouseMoveListener);
+        body.addEventListener('mouseup', mouseUpListener);
     }
 };
 __decorate([
